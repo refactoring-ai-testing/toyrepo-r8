fd0c924f1e2b0390e57fe51b0658bd3ffb40c859
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-20 14:08:48 +0100

    using dog as anonymous class

--- a/A.java
+++ a/A.java
@@ -6,4 +6,16 @@ public class A {
 
 		System.out.println(a);
 	}
+
+	public void m2() {
+		Dog dog = new Dog() {
+	    public void someDog ()
+	      {
+	      	int a = 10;
+	      	System.out.println(a);
+	        System.out.println("Anonymous Dog");
+	      }			
+	    };
+	    dog.someDog();
+	}
 }
